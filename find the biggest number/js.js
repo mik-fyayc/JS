6308ef1c54acc226696f847a64ad1c9ea4f8d54c
bulldozerat <bulldozerat@gmail.com>
2017-05-09 23:04:48 +0300

    Update js.js

--- find the biggest number/js.js
+++ find the biggest number/js.js
@@ -1,6 +1,10 @@
+//Write a JS function that finds the biggest of 3 numbers.
+//The input comes as array of strings, where each element holds a number.
+//The output is the biggest from the input numbers.
+
 function name(array) {
     let nums = array.map(Number);
     console.log(Math.max.apply(Math, nums));
 }
 
-name(['-20','-10','-30','-200']);
+name(['-10','-30','-200']);
