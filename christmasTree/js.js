d4cd6cf0886d04c438fac74ef83eab20fa314037
bulldozerat <bulldozerat@gmail.com>
2017-05-25 19:43:54 +0300

    Add files via upload

---
+++ christmasTree/js.js
@@ -0,0 +1,9 @@
+function christmasTree([arg1]) {
+    let n = Number(arg1);
+
+    for (let i = 0; i < n+1; i++) {
+        console.log(" ".repeat(n-i) + "*".repeat(i) + " | " + "*".repeat(i) + " ".repeat(n-i));
+    }
+}
+
+christmasTree([13]);
